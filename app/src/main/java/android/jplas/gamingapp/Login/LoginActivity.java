1f64f78c7a77dcd1f83be344afde9d4d87082947
AlfalahAnugara <Alfalah190299@gmail.com>
2019-12-17 23:26:19 +0700

    create login and copy paste app plugin

---
+++ app/src/main/java/android/jplas/gamingapp/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package android.jplas.gamingapp.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.jplas.gamingapp.R;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
